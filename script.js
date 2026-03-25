d1fd84c4280c5e8013653821bd21a30c0dfbf7fc
peranten0el <noelgperante@gmail.com>
2026-03-25 23:34:40 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showWelcome() {
+    const element = document.getElementById("welcomeText");
+    if (element) {
+        element.innerHTML = "Hi! I'm Noel G. Perante. Thanks for visiting my page.";
+    }
+}
